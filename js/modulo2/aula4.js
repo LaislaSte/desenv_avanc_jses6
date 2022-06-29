4c2a1d447e446bf2fe4afcc691dd92b08ccfb661
LaislaSte <stephenielaisla@gmail.com>
2022-06-29 17:00:52 -0300

    m2 aula4 corrigido

--- js/modulo2/aula4.js
+++ js/modulo2/aula4.js
@@ -23,16 +23,17 @@ function sum(...args){
     console.log(args)
     return args.reduce((acc, value) => acc + value, 0)
 }
-console.log(sum(5,5, 3,2))
+console.log(sum(5,5, 5,2))
 
 //arguments é inexistentes em arrow funtions, mas com o rest oper dá para utilizar
-const sum = (a, b, ...rest)=>{
+const sum2 = (a, b, ...rest)=>{
     console.log(a, b, rest)
 }
-console.log(4,4, 3,3, 2 )
+sum2(3,3,45,6,7,8,5)
 
-const mult = (...args)=> args.reduce((acc, value) => acc*value, 0)
-console.log(4,4, 3,3, 2 )
+
+const mult = (...args)=> args.reduce((acc, value) => acc * value, 1)
+console.log(mult(5,5,5) )
 
 //spread operator -> pega todos os itens do array e transforma em parametro ou: se usa em string arrays objts literais e objts literaveis
 const str ='hellor diggital innovation'
@@ -55,42 +56,51 @@ const objt = {
 //só pode utilizar para criar novos objetos:
 //se as props forem =, a ordem de quem vem por ultimo prevalece
 const objt2 = {
-    ...obj,
+    ...objt,
     teste2:'hi',
     subobjt:{
-        teste2:'meu subobjt'
+        teste3:123
     }
 }
 console.log(objt2)
+
 //clone de objt -> shallow clone -> nao clona sub objetos 
 const objtClone = {...objt2}
-objtClone.teste = 345 //clona, mas altera o subobjeto
+objtClone.teste = 345 //mudando valor da prop objt passado como sprea
+console.log(objtClone)//nao altera nosso objeto. para nao alterar o subobjt:
 
-console.log(objt2)//nao altera nosso objeto. para nao alterar o subobjt
-const objtClone = {...objt2, subobjt: {...objt2.subobjt} }
+const objtClone2 = {...objt2, subobjt2: {...objt2.subobjt} }
 objtClone.teste = 345
-console.log(objt2) //clona tudo sem alterar nada
+console.log(objtClone2) //clona tudo sem alterar nada
+
 
 //DESTRUCTURING EM REACTJS
 var arra = ['banana', 'maça', 'laranja']
+//var [banana2, maca2, laranja2] = ['banana', 'maça', 'laranja']
 //se quiser pegar cada valor e atribuir a uma var, de modo nao verboso -> destructuring assignment
-var [banana2, maca2, laranja2] = ['banana', 'maça', 'laranja']
-console.log(banana, banana2)//iguais
+var [banana2, maca2, laranja2] = arr
+console.log("comparando a var com o indice do array", banana2, "=", arra[0])//iguais
+
 
 //se colocar um array dentro - 2 niveis
-var arra2 = ['banana', 'maça', 'laranja'['tomate']]
-var [banana2, maca2, laranja2, [tomate2]] = ['banana', 'maça', 'laranja', ['tomate']]
-console.log(tomate, tomate2)//iguais
+var arra2 = ['banana', 'maça', 'laranja', ['tomate', 'pera'] ]
+//var [banana2, maca2, laranja2, [tomate2]] = ['banana', 'maça', 'laranja', ['tomate']]
+var [banana2, maca2, laranja2, [tomate2, pera2]] = arra2
+console.log("comparando a var com o arrar em niveis", tomate2, "=", arra2[3][0])//iguais
+
+
 
 //mesma fita com objts
 var objtDest = {
-    nome:'wergio'
+    nome:'Sergio'
 }
 var { nome: nome2 } = objtDest //acha a prop name e coloca num var name 
-console.log(nome, nome2) //nao muda o valor original
+console.log("comparando", nome2, "=", objtDest.nome) //nao muda o valor original
+
 //nao teria a preocupação com as chaves : se forre um array
 var frutas = ['apple', 'orange'];
 var [apple2] = frutas //log apple2
+
 
 //nested objt dentro de objt
 var pessoa = {
@@ -100,12 +110,16 @@ var pessoa = {
         favcolor: ['preto', 'branco']
     }
 }
-var {propri: {idade: idade2, favcolor: [color1, color2]  } } = pessoa//log ideade2 ou color1 ou 2
+var {propri: {idade: idade2, favcolor: [color1, color2]  } } = pessoa
+
+console.log("comparando", idade2, "=", pessoa.propri.idade)
+console.log("comparando", color1, "=", pessoa.propri.favcolor[0])
+
 
 //se usa com variavel let const var e functions
-
-function sum([a,b] = [0,0]){//default value 0,0
-    return a+b
+/*
+function sum([a,b] = [0, 0]){//default value 0,0
+    console.log(a+b)
     //mema fita: a = arr[0]; b =  arr[1]
 }
 //mema fita com objts
@@ -113,4 +127,4 @@ function sum({a,b}){
     return a+b
     //LOG sum({a:3, b:7})
 }
-
+*/
